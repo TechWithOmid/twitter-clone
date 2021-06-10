95bf3579340c008698767e476f9344194fa508e5
omid <techwithomid@gmail.com>
2021-06-10 16:02:17 +0430

    create tweet detail view and tweet list view

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Tweet
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html")
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweet_list,
+    }
+    return JsonResponse(data)
+
+
+def tweet_detail_view(request, tweet_id):
+    """
+    REST API VIEW
+    """
+    data = {
+        "id": tweet_id,
+    }
+
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
