8521b1f8ea492b3cf2738794ef73de4f9696067f
omid <techwithomid@gmail.com>
2021-07-08 19:19:28 +0430

    add create tweet form

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import TweetForm
 from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html")
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, "components/form.html", context={'form': form})
 
 
 def tweet_list_view(request, *args, **kwargs):
