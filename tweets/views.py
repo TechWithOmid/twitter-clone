269d0272c3264c90f70ed94642f9eaa09f5e35ba
omid <techwithomid@gmail.com>
2021-08-08 18:40:17 +0430

    redirect after tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import TweetForm
@@ -11,10 +11,13 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, "components/form.html", context={'form': form})
 
